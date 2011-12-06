e6231d26bccdbc94fad704d677026cf8c501cf0b
Jamie Wright <jamie@brilliantfantastic.com>
2011-12-06 11:38:15 -0500

    Finished Sinatra app

--- home.rb
+++ home.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+
+get '/css/application.css' do
+  content_type "text/css", :charset => "utf-8"
+  sass :application
+end
 
 get '/' do
   view :home
